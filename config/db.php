75c973204fbc37c8c0425e4df9f36cdab33d1c97
Shashaank MK <mkshashaank425@gmail.com>
2026-02-07 20:00:17 +0530

    Delete config directory

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "canteen_preorder_db");
-
-date_default_timezone_set('Asia/Kolkata');
-
-if ($conn->connect_error) {
-    die("Database Connection Failed: " . $conn->connect_error);
-}
-?>
