765f84c41f05993bb6b59042f96bca5d0f9e3576
Shashaank MK <mkshashaank425@gmail.com>
2026-02-07 20:01:09 +0530

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (isset($_SESSION['role'])) {
-    header("Location: /Canteen-Services/" . $_SESSION['role'] . "/dashboard.php");
-} else {
-    header("Location: /Canteen-Services/login.php");
-}
-exit;
