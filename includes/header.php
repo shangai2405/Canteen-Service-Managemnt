7969518f072ec13a1e6183410cadc80630e5f8c4
Shashaank MK <mkshashaank425@gmail.com>
2026-02-07 20:00:37 +0530

    Delete includes directory

--- includes/header.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Canteen Services</title>
-    <link rel="stylesheet" href="/Canteen-Services/assets/css/style.css">
-</head>
-<body>
--- includes/role_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-function checkRole($requiredRole) {
-    if (!isset($_SESSION['role']) || $_SESSION['role'] !== $requiredRole) {
-        die("❌ Access Denied for this role");
-    }
-}
-?>
